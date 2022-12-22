eab79205292847b8d6d95c1d2e148c25c857961f
abubakarsiddique10 <abubakarsiddique7912@gmail.com>
2022-12-22 15:58:30 +0600

    update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 require('dotenv').config()
-const { connectToServer, getDb, } = require('./utils/dbConnect');
 const { ObjectId } = require('mongodb');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
